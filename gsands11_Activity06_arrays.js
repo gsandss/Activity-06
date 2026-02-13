38f1881ef113ced4f81a8026d0dd9fa801a069a9
gsandss <gavinsands11@gmail.com>
2026-02-13 15:10:24 -0600

    final commit

--- gsands11_Activity06_arrays.js
+++ gsands11_Activity06_arrays.js
@@ -1,1 +1,20 @@
+/*
+Gavin Sands
+Feb 13, 2026
+Activity06 - Arrays
+*/ 
 
+console.log("---- I am in A R R A Y S ----");
+
+// Q1 : Index of each color
+let colors = ['red', 'blue', 'orange']
+
+// Q2 : Add a new element 'green'
+colors[3] = 'green';
+console.log(colors);
+
+colors[4] = 'brown';
+
+// Q3 : Length of the Array
+console.log(colors.length);
+console.log(colors. );
