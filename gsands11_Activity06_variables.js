a9861996488193fba78668a054f936da1c5088cd
gsandss <gavinsands11@gmail.com>
2026-02-13 13:42:37 -0600

    First Commit

---
+++ gsands11_Activity06_variables.js
@@ -0,0 +1,43 @@
+/*
+Gavin Sands
+Feb 13, 2026
+Activity 6: Variables
+*/
+
+console.log("---- I am in V A R I A B L E S ----");
+
+// Q1: Is it permitted the next?
+console.log("Q1 - TODO");
+var var1 = "Iowa";
+console.log(var1);
+
+var var1 = 124;
+console.log(var1);
+
+// Is it Permitted ?
+console.log("TODO(Yes or No)");
+
+// Q2: Is it valid?
+
+console.log("Q2 - TODO");
+let var2 = "Ames";
+console.log(var2);
+var2 = 124;
+
+// Is it valid?
+console.log("TODO(Yes or No)");
+
+// Q3: Is it valid?
+console.log("Q3 - TODO");
+let var3 = "ISU";
+console.log(var3);
+var3= 2023;
+console.log(var3);
+
+console.log("Valid?(Yes or No)");
+
+// Q4: Explain the Error.
+console.log("Q4 - TODO");
+let var4;
+const var5;
+console.log("What is the error:");
