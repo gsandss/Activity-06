0ff52268077b59a7276eb18a1f82cc749df908d5
gsandss <gavinsands11@gmail.com>
2026-02-13 15:01:15 -0600

    operators file commit

---
+++ gsands11_Activity06_operators.js
@@ -0,0 +1,33 @@
+/*
+Gavin Sands
+Feb 13, 2026
+Activity06 - Operators
+*/
+
+console.log("---- I am in O P E R A T O R S ----");
+
+// Q1 : 5 + 9 / 2 + 1
+console.log("Result: " + (5 + 9 / 2 + 1));
+
+// Q2 : 10 / 4 + 7 % 4
+console.log("Result: " + (10 / 4 + 7 % 4));
+
+// Q3 : (5 - 1) * ((7 + 1) / (3 - 1))
+console.log("Result: " + (5 - 1) * ((7 + 1) / (3 - 1)));
+
+// Q4 : 10 / 4 >= 10 / 3
+console.log("Result: " + (10 / 4 >= 10 / 3));
+console.log(">= is a operator to determine if a statement is greater than or equal to another, and results in true or false.")
+
+// Q5 : 10 >= 10 && 10 <= 10
+console.log("Result: " + (10 >= 10 && 10 <= 10));
+console.log("&& is a operator to include one statement AND another to determine if they are both either true or false. So, it will return true if the two statement on each side are either both true or both false.")
+
+// Q6 : Strict Equality
+// (Type AND Value)
+console.log(1 === 1);
+console.log('1' === 1);
+
+// Q7 : Loose Equality
+console.log('1' == 1);
+console.log(true == 1);
